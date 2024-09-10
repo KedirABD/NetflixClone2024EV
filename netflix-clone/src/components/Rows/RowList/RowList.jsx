9e6b69c305a176b76ef02e26cca975971fbf07d3
KedirABD <kedir_abdurohman@yahoo.com>
2024-09-10 12:28:50 +0300

    DeployeUpD

---
+++ netflix-clone/src/components/Rows/RowList/RowList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Row from "../Row/Row";
+import requests from "../../../utils/requests";
+
+function RowList() {
+  return (
+    <>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Ethiopian Movies" fetchUrl={requests.fetchEthiopianMovies} />
+      <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      n{" "}
+      <Row title="Animation Movies" fetchUrl={requests.fetchAnimationMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </>
+  );
+}
+
+export default RowList;
